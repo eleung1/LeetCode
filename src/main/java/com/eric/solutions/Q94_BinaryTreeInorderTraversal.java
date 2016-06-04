950f7fdd82b13396484448ccedaa1f1b4b51fca8
Eric Leung <eric.leung@alumni.utoronto.ca>
2016-06-04 16:45:26 -0400

    Q94 Iterative in-order traversal.

--- src/main/java/com/eric/solutions/Q94_BinaryTreeInorderTraversal.java
+++ src/main/java/com/eric/solutions/Q94_BinaryTreeInorderTraversal.java
@@ -1,7 +1,10 @@
 package com.eric.solutions;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 /**
  * Question 94: Binary Tree Inorder Traversal
@@ -26,7 +29,12 @@ import java.util.List;
  */
 public class Q94_BinaryTreeInorderTraversal
 {
-  
+  /**
+   * Recursive solution.
+   * 
+   * @param root
+   * @return
+   */
   public List<Integer> inorderTraversal(TreeNode root) 
   {
     if ( root == null ) return new LinkedList<Integer>();
@@ -36,6 +44,149 @@ public class Q94_BinaryTreeInorderTraversal
     result.addAll(inorderTraversal(root.right));
     
     return result;
+  }
+  
+  /**
+   * Iterative solution.
+   * 
+   * Idea:
+   * 1.  Traverse to the left most child of this tree.  This will be our first/curr node.
+   * 2.  Find currNode's in-order successor.
+   *     - if currNode has no children
+   *          if currNode is leftChild of parent
+   *             successor is parent
+   *          else if currNode is rightChild of parent
+   *             find first parent who is the left child of its ancestor, that ancestor is the succesor.
+   *     - else if currNode has right child
+   *          successor is left most child of the right child, or the rightChild if the right child has no children.  
+   *     
+   * 
+   * @param root
+   * @return
+   */
+  public List<Integer> inorderTraversalIterative(TreeNode root)
+  {
+    if ( root == null ) return new LinkedList<Integer>();
+    
+    List<Integer> result = new LinkedList<Integer>();
+    
+    // key=child; value=parent
+    Map<TreeNode, TreeNode> parentMap = constructParentMap(root);
+    
+    TreeNode leftMostNode = root;
+    while(leftMostNode.left != null) 
+    {
+      leftMostNode = leftMostNode.left;
+    }
+    
+    result.add(leftMostNode.val);
+    
+    TreeNode successor = getInorderSuccessor(leftMostNode, parentMap);
+    
+    while ( successor != null )
+    {
+      result.add(successor.val);
+      successor = getInorderSuccessor(successor, parentMap);
+    }
+    
+    return result;
+  }
+  
+  /**
+   * The given TreeNode class does not have a reference to its parent.
+   * Explore all nodes(Breadth first traversal) from top to bottom and construct a parent map.
+   * 
+   * @param root Root of this binary tree.
+   * @return A map containing key value pairs of node to parentNode.
+   */
+  public Map<TreeNode, TreeNode> constructParentMap(TreeNode root)
+  {
+    if ( root == null ) return null;
+    
+    Map<TreeNode, TreeNode> parentMap = new HashMap<TreeNode, TreeNode>();
+    Queue<TreeNode> toExplore = new LinkedList<TreeNode>();
+    toExplore.add(root);
+    
+    while ( !toExplore.isEmpty() )
+    {
+      TreeNode currNode = toExplore.poll();
+      if ( currNode.left != null )
+      {
+        parentMap.put(currNode.left, currNode);
+        toExplore.add(currNode.left);
+      }
+      
+      if ( currNode.right != null )
+      {
+        parentMap.put(currNode.right, currNode);
+        toExplore.add(currNode.right);
+      }
+    }
+    
+    return parentMap;
+  }
+  
+  /**
+   * Helper method to find the in-order successor of a given node.
+   * 
+   * @param node
+   * @return
+   */
+  public TreeNode getInorderSuccessor(TreeNode node, Map<TreeNode, TreeNode> parentMap)
+  {
+    if ( node == null ) return null;
+    
+    TreeNode parent = parentMap.get(node);
+    
+    if ( node.right == null)
+    {
+      // does not have right child and currNode is the leftChild of parent
+      if ( parent != null && parent.left == node ) return parent;
+      
+      if ( parent != null && parent.right == node )
+      {
+        // in this case we have already visited parent
+        // return first parent who is a left child of its parent
+        TreeNode successor = parentMap.get(parent);
+        while ( successor != null )
+        {
+          if ( successor.left == parent )
+          {
+            return successor;
+          }
+          parent = successor;
+          successor = parentMap.get(successor);
+        }
+        return null; // cannot find a successor, we are at the right most node of the tree, i.e. done.
+      }
+    }
+    else
+    {
+      // return left most child of node.right
+      return findLeftMostChild(node.right);
+    }
+    
+    return null;
+    
+  }
+  
+  /**
+   * Helper method to find the left most child of the tree rooted at the given node.
+   * 
+   * @param node
+   * @return
+   */
+  public TreeNode findLeftMostChild(TreeNode node)
+  {
+    if ( node == null ) return null;
+    
+    TreeNode leftMostChild = node;
+    while ( leftMostChild.left != null )
+    {
+      leftMostChild = leftMostChild.left;
+    }
+    
+    return leftMostChild;
   }
   
   /**
--- src/test/java/com/eric/solutions/Q94_BinaryTreeInorderTraversalTest.java
+++ src/test/java/com/eric/solutions/Q94_BinaryTreeInorderTraversalTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 import com.eric.solutions.Q94_BinaryTreeInorderTraversal.TreeNode;
 
 public class Q94_BinaryTreeInorderTraversalTest
@@ -13,6 +15,12 @@ public class Q94_BinaryTreeInorderTraversalTest
   
   private TreeNode root1;
   
+  private TreeNode root2;
+  
+  private TreeNode root3;
+  
+  private TreeNode root4;
+  
   @Before
   public void setup()
   {
@@ -20,6 +28,21 @@ public class Q94_BinaryTreeInorderTraversalTest
     root1 = testSubject.new TreeNode(1);
     root1.right = testSubject.new TreeNode(2);
     root1.right.left = testSubject.new TreeNode(3);
+    
+    root2 = testSubject.new TreeNode(1);
+    root2.left = testSubject.new TreeNode(2);
+    root2.left.left = testSubject.new TreeNode(3);
+    root2.right = testSubject.new TreeNode(4);
+    root2.right.left = testSubject.new TreeNode(5);
+    
+    root3 = testSubject.new TreeNode(2);
+    root3.right = testSubject.new TreeNode(3);
+    root3.right.right = testSubject.new TreeNode(1);
+    
+    root4 = testSubject.new TreeNode(3);
+    root4.left = testSubject.new TreeNode(4);
+    root4.left.right = testSubject.new TreeNode(2);
+    root4.left.right.right = testSubject.new TreeNode(1);
   }
   
   @Test
@@ -27,9 +50,30 @@ public class Q94_BinaryTreeInorderTraversalTest
   {
     List<Integer> rc = testSubject.inorderTraversal(root1);
     
-    for ( int i: rc )
-    {
-      System.out.println(i);
-    }
+    assertArrayEquals(new Integer[]{1,3,2}, rc.toArray(new Integer[0]));
+  }
+  
+  @Test
+  public void testInorderTraversalIterative()
+  {
+    List<Integer> rc = testSubject.inorderTraversalIterative(root2);
+    
+    assertArrayEquals(new Integer[]{3,2,1,5,4}, rc.toArray(new Integer[0]));
+  }
+  
+  @Test
+  public void testInorderTraversalIterative_rightBranchOnly()
+  {
+    List<Integer> rc = testSubject.inorderTraversalIterative(root3);
+    
+    assertArrayEquals(new Integer[]{2,3,1}, rc.toArray(new Integer[0]));
+  }
+  
+  @Test
+  public void testInorderTraversalIterative_leftBranchOnly()
+  {
+    List<Integer> rc = testSubject.inorderTraversalIterative(root4);
+    
+    assertArrayEquals(new Integer[]{4,2,1,3}, rc.toArray(new Integer[0]));
   }
 }
