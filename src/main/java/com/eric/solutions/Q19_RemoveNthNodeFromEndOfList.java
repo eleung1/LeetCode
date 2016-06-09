d49602e93a6f8b3ee5f30d557d2895df0567c969
eleung <eric.leung@bellmedia.ca>
2016-06-09 13:49:53 -0400

    Q19 Recursive solution.

--- src/main/java/com/eric/solutions/Q19_RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/eric/solutions/Q19_RemoveNthNodeFromEndOfList.java
@@ -21,9 +21,11 @@ public class Q19_RemoveNthNodeFromEndOfList
 {
   /**
    * Idea: create a "window" with a start node and and end node
-   *       that is n-1 nodes apart.
+   *       that is n nodes apart.
    *       When the end node reaches the end of the list, our start
-   *       node is at the node we want to delete.  
+   *       node is at the node previous to the node we want to delete.
+   *       We can simply set the start node's next reference to next.next
+   *       and we're done.  
    * 
    * @param head
    * @param n
@@ -31,39 +33,26 @@ public class Q19_RemoveNthNodeFromEndOfList
    */
   public ListNode removeNthFromEnd(ListNode head, int n) 
   {
-    ListNode start = head;
-    ListNode end = head;
-    if ( n == 1 )
-    {
-      // Special case, delete last node from list.
-      if ( start.next == null ) 
-      {
-        return null;  // list with one node
-      }
-      else
-      {
-        end = start.next;
-        while ( end.next != null )
-        {
-          start = start.next;
-          end = end.next;
-        }
-        start.next = null;
-      }
-      return head;
-    }
+    if ( head == null || n < 1 ) return null; // Illegal arguments
+    
+    ListNode start,end;
+    start = end = head;
+    
+    // list with one node
+    if ( n == 1 && start.next == null) return null;
     
     // # of hops between start and end node.
-    int distance = n - 1; 
-    start = head; // start window
-    end = head;   // end window
-    for ( int i = 0; i < distance; i ++ )
+    for ( int i = 0; i < n; i ++ )
     {
       end = end.next;
     }
     
-    // Advance of rolling window of size n forward.
-    if ( end != null )
+    if ( end == null )
+    {
+      // if end is null after making n hops, that means we need to remove head
+      return head.next;
+    }
+    else
     {
       while ( end.next != null )
       {
@@ -72,8 +61,7 @@ public class Q19_RemoveNthNodeFromEndOfList
       }
     }
     
-    // end node reached the end of list, which means start node is at the nth node.
-    start.val = start.next.val;
+    // end node reached the end of list, which means start node is at the nth+1 node from the end.
     start.next = start.next.next;
     
     return head;
@@ -122,6 +110,88 @@ public class Q19_RemoveNthNodeFromEndOfList
   }
   
   /**
+   * Recursive solution.
+   * 
+   * Idea: Have a wrapper class around ListNode that keeps track of the position
+   * of the current node from the end.
+   * 
+   * Base case: if we have fell off the list (i.e. head is null, then return 0 because we are 0 nodes away from end)
+   * 1:  If we are at the last node, return 1
+   * 2:  If we are at the 2nd last node, return 2
+   * 
+   * If the return value == n, we have found our node nth position from the end of list
+   * 
+   * @param currNodeWrapper
+   * @param n
+   * @param curr
+   * @return
+   */
+  public ListNodeWrapper removeNthFromEndRecursive(ListNodeWrapper currNodeWrapper, int n)
+  {
+    if ( currNodeWrapper.node == null ) return new ListNodeWrapper(currNodeWrapper.node, currNodeWrapper.head, 0);
+    
+    ListNodeWrapper nextNodeWrapper = removeNthFromEndRecursive(new ListNodeWrapper(currNodeWrapper.node.next, currNodeWrapper.head, 0), n);
+    if ( nextNodeWrapper.pos == n ) 
+    { 
+      // next node is the node we want to remove, simply set currNode's next to skip over the "removed" node.
+      if ( currNodeWrapper.node.next == nextNodeWrapper.node) currNodeWrapper.node.next = nextNodeWrapper.node.next;
+    }
+    
+    // Curr node's position from end is next Node's position from end plus 1
+    int currPosFromEnd = nextNodeWrapper.pos + 1;
+    
+    // if head node is the nth from end, set head to be head.next
+    if ( currPosFromEnd == n && currNodeWrapper.node == currNodeWrapper.head )
+    {
+      // we have to remove head here because this is the end of the recursive calls.
+      currNodeWrapper.head = currNodeWrapper.head.next;
+    }
+    
+    return new ListNodeWrapper(currNodeWrapper.node, currNodeWrapper.head, currPosFromEnd);
+  }
+  
+  /**
+   * Entry point into our recursive method
+   * 
+   * @param head
+   * @param n
+   * @return
+   */
+  public ListNode removeNthFromEndRecursiveEntry(ListNode head, int n) 
+  {
+    if ( head == null || n < 1 ) return null; // Illegal arguments
+    
+    // list with one node
+    if ( n == 1 && head.next == null) return null;
+    
+    ListNodeWrapper headWrapper = removeNthFromEndRecursive(new ListNodeWrapper(head, head, 0), n);
+    
+    return headWrapper.head;
+  }
+  
+  /**
+   * Experimental method that will print out the nth node from end, just to get some ideas flowing.
+   * 
+   * @param head
+   * @param n
+   * @return
+   */
+  public int removeNthFromEndRecursive(ListNode head, int n)
+  {
+    if ( head == null ) return 0;
+    
+    int currPos = removeNthFromEndRecursive(head.next, n) + 1;
+    
+    if ( currPos == n) 
+    {
+      System.out.println(head.val);
+      return currPos;
+    }
+    
+    return currPos;
+  }
+  
+  /**
    * Definition for singly-linked list defined by the question.
    */
    public class ListNode 
@@ -131,4 +201,22 @@ public class Q19_RemoveNthNodeFromEndOfList
      ListNode(int x) { val = x; }
    }
    
+   /**
+    * Wraps the node object with its position from end of list.
+    * 
+    * Used in the recursive solution.
+    *
+    */
+   public class ListNodeWrapper
+   {
+     int pos; // position from the end of list.
+     ListNode head; // need a ref to head, so we can remove head if it is the nth node from end
+     ListNode node; // node is the current node being processed as we unwind the recursive calls
+     ListNodeWrapper(ListNode n, ListNode h, int p)
+     {
+       node = n;
+       head = h;
+       pos = p;
+     }
+   }
 }
--- src/test/java/com/eric/solutions/Q19_RemoveNthNodeFromEndOfListTest.java
+++ src/test/java/com/eric/solutions/Q19_RemoveNthNodeFromEndOfListTest.java
@@ -11,13 +11,16 @@ public class Q19_RemoveNthNodeFromEndOfListTest
 {
   private Q19_RemoveNthNodeFromEndOfList testSubject;
   private ListNode head1;
-  private ListNode head_singleNode;
+  private ListNode head_singleNodeList;
+  private ListNode head_twoNodesList;
   @Before
   public void setup()
   {
     testSubject = new Q19_RemoveNthNodeFromEndOfList();
     
-    head_singleNode = testSubject.new ListNode(1);
+    head_singleNodeList = testSubject.new ListNode(1);
+    
+    
     
     head1 = testSubject.new ListNode(1);
     head1.next = testSubject.new ListNode(2);
@@ -25,21 +28,33 @@ public class Q19_RemoveNthNodeFromEndOfListTest
     head1.next.next.next = testSubject.new ListNode(4);
     head1.next.next.next.next = testSubject.new ListNode(5);
     
-    head_singleNode = testSubject.new ListNode(1);
+    head_singleNodeList = testSubject.new ListNode(1);
+    
+    head_twoNodesList = testSubject.new ListNode(1);
+    head_twoNodesList.next = testSubject.new ListNode(2);
   }
   
   @Test
   public void testRemoveNthFromEnd_givenSingleNode_returnNull()
   {
-    ListNode head = testSubject.removeNthFromEnd(head_singleNode, 1);
+    ListNode head = testSubject.removeNthFromEnd(head_singleNodeList, 1);
     assertNull(head);
   }
   
   @Test
-  public void testRemoveNthFromEndNaive_givenSingleNode_returnNull()
+  public void testRemoveNthFromEnd_given2NodeListRemove2ndNode_return1stNode()
   {
-    ListNode head = testSubject.removeNthFromEndNaive(head_singleNode, 1);
-    assertNull(head);
+    ListNode head = testSubject.removeNthFromEnd(head_twoNodesList, 1);
+    assertEquals(1, head.val);
+    assertNull(head.next);
+  }
+  
+  @Test
+  public void testRemoveNthFromEnd_given2NodeListRemove1stNode_return2ndNode()
+  {
+    ListNode head = testSubject.removeNthFromEnd(head_twoNodesList, 2);
+    assertEquals(2, head.val);
+    assertNull(head.next);
   }
   
   @Test
@@ -62,6 +77,14 @@ public class Q19_RemoveNthNodeFromEndOfListTest
     
   }
   
+  // --- Naive solution test
+  @Test
+  public void testRemoveNthFromEndNaive_givenSingleNode_returnNull()
+  {
+    ListNode head = testSubject.removeNthFromEndNaive(head_singleNodeList, 1);
+    assertNull(head);
+  }
+  
   @Test
   public void testRemoveNthFromEndNaive_givenProperParams_returnProperResult()
   {
@@ -81,4 +104,54 @@ public class Q19_RemoveNthNodeFromEndOfListTest
     assertNull(head.next.next.next);
     
   }
+  
+  // --- Recursive solution tests
+  
+  @Test
+  public void testRemoveNthFromEndRecursive_givenProperList_printOut4()
+  {
+    // This method simply prints out the value of the node that is nth position from the end
+    // 1 > 2 > 3 > 4 > 5
+    // Remove 2nd from end (4), result list is 1 > 2 > 3 > 5
+    testSubject.removeNthFromEndRecursive(head1, 2);
+  }
+  
+  @Test
+  public void testRemoveNthFromEndRecursive_givenProperList_returnListWithNthNodeRemoved()
+  {
+    // 1 > 2 > 3 > 4 > 5
+    // Remove 2nd from end (4), result list is 1 > 2 > 3 > 5
+    ListNode head = testSubject.removeNthFromEndRecursiveEntry(head1, 2);
+    
+    assertEquals(1, head.val);
+    assertEquals(2, head.next.val);
+    assertEquals(3, head.next.next.val);
+    assertEquals(5, head.next.next.next.val);
+    assertNull(head.next.next.next.next);
+    
+  }
+  
+  @Test
+  public void testRemoveNthFromEndRecursive_givenSingleNode_returnNull()
+  {
+    ListNode head = testSubject.removeNthFromEndRecursiveEntry(head_singleNodeList, 1);
+    assertNull(head);
+  }
+  
+  @Test
+  public void testRemoveNthFromEndRecursive_given2NodeListRemove2ndNode_return1stNode()
+  {
+    ListNode head = testSubject.removeNthFromEndRecursiveEntry(head_twoNodesList, 1);
+    assertEquals(1, head.val);
+    assertNull(head.next);
+  }
+  
+  @Test
+  public void testRemoveNthFromEndRecursive_given2NodeListRemove1stNode_return2ndNode()
+  {
+    ListNode head = testSubject.removeNthFromEndRecursiveEntry(head_twoNodesList, 2);
+    assertEquals(2, head.val);
+    assertNull(head.next);
+  }
+  
 }
