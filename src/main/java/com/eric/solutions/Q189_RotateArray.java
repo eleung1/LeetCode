16ea4da8a1f0ceda8ae743a9105e052452352058
Eric Leung <eric.leung@alumni.utoronto.ca>
2016-07-09 23:31:25 -0400

    Q189 Rotate array.

--- src/main/java/com/eric/solutions/Q189_RotateArray.java
+++ src/main/java/com/eric/solutions/Q189_RotateArray.java
@@ -5,7 +5,8 @@ package com.eric.solutions;
  * 
  * Rotate an array of n elements to the right by k steps.
  * 
- * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to
+ * For example, with n = 7 and k = 3, the array 
+ * [1,2,3,4,5,6,7] is rotated to
  * [5,6,7,1,2,3,4].
  * 
  * Note: Try to come up as many solutions as you can, there are at least 3
@@ -18,6 +19,16 @@ package com.eric.solutions;
  */
 public class Q189_RotateArray
 {
+  /**
+   * Cloning the array, then simply update the array elements
+   * base on their shifted index.
+   * 
+   * Time O(n)
+   * Space O(n)
+   *
+   * @param nums
+   * @param k
+   */
   public void rotate(int[] nums, int k) 
   {
     int[] numsClone = nums.clone();
@@ -28,4 +39,88 @@ public class Q189_RotateArray
     }
     
   }
+  
+  /**
+   * Bubble up the last element to index 0, which has 
+   * the same effect has rotating the array 1 step to the right.
+   * 
+   * Do this k times, and we have rotated the array k-steps to the right.
+   * 
+   * Time: O(n*k)
+   * Space: O(1)
+   * 
+   * @param nums
+   * @param k
+   */
+  public void rotateBubbleUp(int[] nums, int k)
+  {
+    while ( k > 0 )
+    {
+      for ( int i = nums.length - 1; i > 0; i -- )
+      {
+        int temp = nums[i-1];
+        nums[i-1] = nums[i];
+        nums[i] = temp;
+      }
+      k--;
+    }
+  }
+  
+  /**
+   * Split the array into 2 parts
+   * 
+   * [0 to length-k-1] | [length-k to length-1]
+   * 
+   * Example:
+   * Given:    [1 2 3 4 5]   k=2
+   * Expected: [4 5 1 2 3]
+   * 
+   * 1.  Split array into:
+   * [1 2 3] [4 5]
+   * 
+   * 2.  Reverse both sections:
+   * [3 2 1] [5 4]
+   * 
+   * 3.  Reverse whole array(done):
+   * [4 5 1 2 3]
+   * 
+   * Time: O(n)
+   * Space: O(1)
+   * 
+   * @param nums
+   * @param k
+   */
+  public void rotateWithReverse(int[] nums, int k)
+  {
+    if ( k > nums.length ) k = k%nums.length;
+    
+    // reverse 0 to length-k-1
+    reverseHelper(nums, 0, nums.length-k);
+    
+    // reverse length-k to length
+    reverseHelper(nums, nums.length-k, nums.length);
+    
+    // reverse whole array
+    reverseHelper(nums, 0, nums.length);
+  }
+  
+  /**
+   * Helper method that reverse a portion of the array between
+   * index start and index end.
+   * 
+   * @param nums The array
+   * @param start The start index (inclusive)
+   * @param end The end index (exclusive)
+   */
+  public void reverseHelper(int[] nums, int start, int end)
+  {
+    for ( int i = start; i < (start+end) / 2; i++ )
+    {
+      int temp = nums[i];
+      nums[i] = nums[end-i+start-1];
+      nums[end-i+start-1] = temp;
+    }
+  }
+  
+  
 }
--- src/test/java/com/eric/solutions/Q189_RotateArrayTest.java
+++ src/test/java/com/eric/solutions/Q189_RotateArrayTest.java
@@ -11,7 +11,8 @@ public class Q189_RotateArrayTest
   private int[] nums;
   private int[] numsSize1;
   private int[] numsSize2;
-  
+  private int[] numsSize6;
+ 
   @Before
   public void setup()
   {
@@ -19,6 +20,7 @@ public class Q189_RotateArrayTest
     nums = new int[]{1,2,3,4,5,6,7};
     numsSize1 = new int[]{1};
     numsSize2 = new int[]{1,2};
+    numsSize6 = new int[]{1,2,3,4,5,6};
   }
   
   @Test
@@ -32,5 +34,78 @@ public class Q189_RotateArrayTest
     
     testSubject.rotate(numsSize2, 3);
     assertArrayEquals(new int[]{2,1}, numsSize2);
+    
+    testSubject.rotate(numsSize6, 2);
+    assertArrayEquals(new int[]{5,6,1,2,3,4}, numsSize6);
+  }
+  
+  @Test
+  public void testWithBubbleUp()
+  {
+    
+    testSubject.rotateBubbleUp(numsSize1, 1);
+    assertArrayEquals(new int[]{1}, numsSize1);
+    
+    testSubject.rotateBubbleUp(nums, 3);
+    assertArrayEquals(new int[]{5,6,7,1,2,3,4}, nums);
+    
+    testSubject.rotateBubbleUp(numsSize2, 3);
+    assertArrayEquals(new int[]{2,1}, numsSize2);
+    
+    testSubject.rotateBubbleUp(numsSize6, 2);
+    assertArrayEquals(new int[]{5,6,1,2,3,4}, numsSize6);
+    
+  }
+  
+  @Test
+  public void testWithReverse()
+  {
+    
+    testSubject.rotateWithReverse(numsSize1, 1);
+    assertArrayEquals(new int[]{1}, numsSize1);
+    
+    testSubject.rotateWithReverse(nums, 3);
+    assertArrayEquals(new int[]{5,6,7,1,2,3,4}, nums);
+    
+    testSubject.rotateWithReverse(numsSize2, 3);
+    assertArrayEquals(new int[]{2,1}, numsSize2);
+    
+    testSubject.rotateWithReverse(numsSize6, 2);
+    assertArrayEquals(new int[]{5,6,1,2,3,4}, numsSize6);
+    
+  }
+  
+  @Test
+  public void testReverseHelper()
+  {
+    testSubject.reverseHelper(nums, 0, nums.length);
+    assertArrayEquals(new int[]{7,6,5,4,3,2,1}, nums);
+    
+    testSubject.reverseHelper(numsSize1, 0, numsSize1.length);
+    assertArrayEquals(new int[]{1}, numsSize1);
+    
+    testSubject.reverseHelper(numsSize2, 0, numsSize2.length);
+    assertArrayEquals(new int[]{2,1}, numsSize2);
+    
+  }
+  
+  @Test
+  public void testReverseHelper2()
+  {
+    int[] myNums = new int[]{1,2,3,4,5,6,7};
+    testSubject.reverseHelper(myNums, 0, 2);
+    assertArrayEquals(new int[]{2,1,3,4,5,6,7}, myNums);
+    
+    myNums = new int[]{1,2,3,4,5,6,7};
+    testSubject.reverseHelper(myNums, 0, 1);
+    assertArrayEquals(new int[]{1,2,3,4,5,6,7}, myNums);
+    
+    myNums = new int[]{1,2,3,4,5,6,7};
+    testSubject.reverseHelper(myNums, 1, 3);
+    assertArrayEquals(new int[]{1,3,2,4,5,6,7}, myNums);
+    
+    myNums = new int[]{1,2,3,4,5,6,7};
+    testSubject.reverseHelper(myNums, 4, 7);
+    assertArrayEquals(new int[]{1,2,3,4,7,6,5}, myNums);
   }
 }
